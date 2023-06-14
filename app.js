f434c81fe2a855e761d4417c4c1682ea0d0c191b
Andrew <andrewallison5211@gmail.com>
2023-06-14 16:54:19 -0600

    Almost Full Function

--- app.js
+++ app.js
@@ -16,7 +16,28 @@ const iceCream = [{
   image: 'https://www.realfoodwithjessica.com/wp-content/uploads/2017/07/paleostrawberryicecream2.jpg',
   price: 1.25,
   quantity: 0
+}]
 
+const toppings = [{
+  name: 'Sprinkles',
+  image: 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Sprinkles2.jpg',
+  price: .25,
+  quantity: 0,
+}, {
+  name: 'Chocolate Chips',
+  image: 'https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/chocolate-chips.jpg?quality=82&strip=1&resize=640%2C360',
+  price: .25,
+  quantity: 0,
+}]
+
+const containers = [{
+  name: 'Waffle Cone',
+  image: 'https://m.media-amazon.com/images/I/71VNjBMakfL._SL1500_.jpg',
+  price: 2
+}, {
+  name: 'Waffle Bowl',
+  image: 'http://images.wbmason.com/350/L_JOY66050.jpg',
+  price: 4
 }]
 
 // Selection Functions
@@ -52,6 +73,25 @@ function clickStrawberry() {
   drawTotal()
 }
 
+function clickSprinkles() {
+  let sprinkleTopping = toppings.find(sprinkleTopping => sprinkleTopping.name == 'Sprinkles')
+  // @ts-ignore
+  sprinkleTopping.quantity++
+  console.log(sprinkleTopping);
+  drawCart()
+  drawTotal()
+}
+
+function clickChocolateChips() {
+  let chipTopping = toppings.find(chipTopping => chipTopping.name == 'Chocolate Chips')
+  // @ts-ignore
+  chipTopping.quantity++
+
+  drawCart()
+  drawTotal()
+}
+
+
 
 function drawCart() {
 
@@ -63,8 +103,9 @@ function drawCart() {
       <li>
             <div class="d-flex justify-content-between">
               <span>${iceCreamFlavors.name}</span>
+              <span>${iceCreamFlavors.price.toFixed(2)}</span>
               <span>${iceCreamFlavors.quantity}</span>
-              <span>${iceCreamFlavors.price}</span>
+              
             
             </div>
           </li>
@@ -72,9 +113,27 @@ function drawCart() {
     }
   })
 
+  let stringofToppings = ''
+
+  toppings.forEach(toppingFlavors => {
+    if (toppingFlavors.quantity > 0) {
+      stringofToppings += `
+      <li>
+      <div class="d-flex justify-content-between">
+      <span>${toppingFlavors.name}</span>
+      <span>${toppingFlavors.price}</span>
+      <span>${toppingFlavors.quantity}</span>
+      
+      
+      </div>
+      </li>
+      `
+    }
+  })
+
   const cartElement = document.getElementById('cart')
   // @ts-ignore
-  cartElement.innerHTML = stringOfIceCreamFlavors
+  cartElement.innerHTML = stringOfIceCreamFlavors + stringofToppings
 }
 
 
@@ -83,6 +142,10 @@ function drawTotal() {
 
   iceCream.forEach(iceCreamFlavor => {
     cartSum += iceCreamFlavor.price * iceCreamFlavor.quantity
+  })
+
+  toppings.forEach(toppingFlavor => {
+    cartSum += toppingFlavor.price * toppingFlavor.quantity
   })
 
   let cartTotalElement = document.getElementById('cartTotal')
